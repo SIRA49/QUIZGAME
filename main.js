4f6823222bf85dc30109c7bd115a81aff5945cda
SIRA49 <85667257+SIRA49@users.noreply.github.com>
2021-07-25 02:21:24 +0900

    Add files via upload

--- main.js
+++ main.js
@@ -2,13 +2,16 @@ enchant();
 var lock    = false;
 var doorsound =false;
 var Q1=0,Q2=0,Q3=0,a=0,b=0,re1,re2,re3,score=0;
-var aButton = new Array(2);
+
+//クイズの問題を配列に入れている。
 var quiz1=["日本の国鳥はキジである。○か×か？","日本の国花は桜と菊の２つである。〇か×か？","日本で一番大きな湖は霞ヶ浦である。〇か×か？"];
 var answer1=[1,1,2];
 var quiz2=["日本で一番長い川は信濃川である。〇か×か？","世界最大の動物は象である。〇か×か？","年齢を重ね目が老化して発症する症状を遠視と呼ぶ。〇か×か？"];
 var answer2=[1,2,2];
 var quiz3=["海のミルクと呼ぶのは「鯛（タイ）」のことである。〇か×か？","干支の巳（み）の次は未（ひつじ）である。〇か×か？","お寿司屋さんではお茶のことを「あがり」と言う。〇か×か？"];
 var answer3=[2,1,1];
+
+//randomにより、各ステージごとの出題問題が毎回変わる。
 var random1 = Math.floor( Math.random() * quiz1.length );
 var random2 = Math.floor( Math.random() * quiz2.length );
 var random3 = Math.floor( Math.random() * quiz3.length );
@@ -18,6 +21,8 @@ var MAP  	    = "map0.gif" ;
 var PLAYER          = "player.gif";
 var BIGDOOR         = "adaa.gif";
 var DOOR            = "自動ドアが開く.mp3";
+
+//各部品をASSETSにまとめている。
 var ASSETS = [
     DOOR,textfiled,MAP,PLAYER,BIGDOOR,TEXT
 ];
@@ -37,7 +42,8 @@ window.onload = function() {
         player.isMoving = false;
         player.direction = 0;
         player.walk = 1;
-	
+
+	//signは○の標識
 	sign = new Sprite(32, 32);
         sign.x = 96;
         sign.y = 93;
@@ -46,6 +52,7 @@ window.onload = function() {
 	sign.frame=9;
         sign.image = game.assets[MAP];
 
+	//sign1は×の標識
 	sign1 = new Sprite(32, 32);
         sign1.x = 192;
         sign1.y = 93;
@@ -54,6 +61,7 @@ window.onload = function() {
 	sign1.frame=10;
         sign1.image = game.assets[MAP];
 
+　　　　//buttonyellowは四角い黄色
 	buttonyellow = new Sprite(16, 16);
 	buttonyellow.x = 128;
         buttonyellow.y = 156;
@@ -62,6 +70,7 @@ window.onload = function() {
 	buttonyellow.frame=10;
         buttonyellow.image = game.assets[MAP];
 
+　　　　//buttonredは四角い赤
 	buttonred = new Sprite(16, 16);
 	buttonred.x = 193;
         buttonred.y = 30;
@@ -69,7 +78,8 @@ window.onload = function() {
 	buttonred.scaleY = 1;
 	buttonred.frame=11;
         buttonred.image = game.assets[MAP];
-         
+
+        //doorはドア
 	door = new Sprite(32,32);
 	door.x = 143;
         door.y = -4;
@@ -78,7 +88,8 @@ window.onload = function() {
 	door.frame=6;
         door.image = game.assets[BIGDOOR];
 	door.open = [13,6];
-        
+
+        //windowは上の青のテキストフィールド
 	var window = new Sprite();
         window.image = game.assets[textfiled];
         window.moveTo(15, -24);
@@ -87,6 +98,7 @@ window.onload = function() {
 	window.scaleX = 1.1;
 	window.scaleY = 0.55; 
 
+　　　　//textは右下のテキストフィールド
 	var text    = new Sprite();
 	text.image = game.assets[TEXT];
         text.moveTo(205, 185);
@@ -94,7 +106,8 @@ window.onload = function() {
         text.height = 160;
 	text.scaleX = 0.45;
 	text.scaleY = 0.20;
-	
+
+	//myLabelは上のテキストフィールドの文字
 	var myLabel = new Label(game.score);
 	myLabel.font = "16px";
 	myLabel.color = "black";
@@ -103,6 +116,7 @@ window.onload = function() {
 	myLabel.scaleX = 1;
 	myLabel.scaleY = 1.5;
 
+　　　　//myLabelは右下のテキストフィールドの文字
 	var myLabel1 = new Label(game.score);
 	myLabel1.font = "16px";
 	myLabel1.color = "black";
@@ -114,19 +128,14 @@ window.onload = function() {
             if (this.isMoving) {
                 this.moveBy(this.vx, this.vy);
                 this.walk = game.frame % 3;
-/*
-                if (!(game.frame % 3)) {
-                    this.walk++;
-                    this.walk %= 3;
-                }
-*/
+
                 if ((this.vx && this.x % 16 == 0) || (this.vy && (this.y-8) % 16 == 0)) {
                     this.isMoving = false;
-                    //this.walk = 1;
+                   
                 }
             } else {
                 this.vx = this.vy = 0;
-                //var n = Math.random();
+ 
                 if (game.input.left) {
                     this.direction = 1;
                     this.vx = -4;
@@ -140,9 +149,9 @@ window.onload = function() {
                     this.direction = 0;
                     this.vy = 4;
                 }
-               
+               /* inputが与えられている時のみ(値が0以外なら中身の処理を行う) */
                 if (this.vx || this.vy) {
-                   
+                   /* フィールド内のみ移動可能とするための処理 */
                     var x = this.x + (this.vx ? this.vx / Math.abs(this.vx) * 16 : 0) + 8;
                     var y = this.y + (this.vy ? this.vy / Math.abs(this.vy) * 16 : 0) + 8;
                     if (0 <= x && x < map.width && 0 <= y && y < map.height && !map.hitTest(x, y)) {
@@ -175,13 +184,15 @@ window.onload = function() {
             var y = Math.min((game.height - 32) / 2 - player.y, 0);
 			x = Math.max(game.width,  x + map.width)  - map.width;
 			y = Math.max(game.height, y + map.height) - map.height;
-          
+          /* プレーヤの位置に応じて画面上のmapをスクロールさせる処理 */
             stage.x = x;
             stage.y = y;
         };
-		player.addEventListener(Event.ENTER_FRAME, function(){
-		
 
+		//addEventListenerで様々処理を行う
+		player.addEventListener(Event.ENTER_FRAME, function(){　
+		
+			//プレイヤーがsignに触れたときの処理
 			if (player.intersect(sign)){
 			if(lock==false){
 			myLabel.text = "黄色の感圧版を踏んで、問題を見てください!!!";
@@ -197,6 +208,8 @@ window.onload = function() {
 					}	
 				}
 			}
+		
+		//プレイヤーがsign1に触れたときの処理
 		if (player.intersect(sign1)){
 			if(lock==false){
 			myLabel.text = "黄色の感圧版を踏んで、問題を見てください!!!";
@@ -213,6 +226,7 @@ window.onload = function() {
 				}
 			}
 
+		//プレイヤーがbuttonyellowに触れたときの処理
 		 if (player.intersect(buttonyellow)){
 		if(b==0){
 		lock=true;
@@ -226,7 +240,9 @@ window.onload = function() {
 				}
 			}
 
+	//スプライトの画像を更新するために必用
 	door.onenterframe = function(){
+		//プレイヤーがbuttonredに触れたときの処理
 		if (player.intersect(buttonred)){
 			if((Q1==1||Q1==2)&&a==0){
 				this.frame = this.open[0];
@@ -260,18 +276,18 @@ window.onload = function() {
 				a=7;
 				}
 			       }
+			　　}
+	　　　　　　　if(doorsound==true){
+　　　　　　　	　　game.assets[DOOR].play();
+　　　　　　　	　doorsound=false;
+	 　　　　}
 		}
-		if(doorsound==true){
-	game.assets[DOOR].play();
-	doorsound=false;
-	}
-		}
 
 
 
 
 
-
+　　　　　　　　//プレイヤーがdoorに触れたときの処理
 		if (player.intersect(door)){
 
 			if(a==1){
@@ -342,11 +358,12 @@ window.onload = function() {
 		}
 
 
-	});
+	});//ここまでがaddイベントリスナーの処理
 };
     game.start();
 };
 
+//gameOverに必用
 var gameOver = function(re1,re2,re3,score, rst) {
     // メッセージ作成
     var msg = "第１問:"+re1+" 第２問:"+re2+ " 第３問:"+re3+" 結果"+score + "point! " + rst;
@@ -354,11 +371,12 @@ var gameOver = function(re1,re2,re3,score, rst) {
     alert(msg);
 };
 
-var setupStage = function(stageIndex)  //?e?X?e?[?W?2?A?I?}?b?v
-{ 
+//マップのデータを４マップ分、格納している
+var setupStage = function(stageIndex)  
     var stage=stageIndex;
     
-	if(stage==0){   //stage1
+	if(stage==0){ //stage1
+	   //マップの読み込み
 	 map.loadData([
 	[ 3, 3, 3, 3, 3, 3, 3, 3, 3 ,5, 5, 3, 3, 3, 3, 3, 3, 3, 3, 3],
         [ 3, 3, 3, 3, 3, 3, 3, 3, 3 ,5, 5, 3, 3, 3, 3, 3, 3, 3, 3, 3],
@@ -375,6 +393,7 @@ var setupStage = function(stageIndex)  //?e?X?e?[?W?2?A?I?}?b?v
         [ 3, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 3],
         [ 3, 3, 3, 3, 3, 3, 3, 3, 3, 5, 5, 3, 3, 3, 3, 3, 3, 3, 3, 3]
         ]);
+	//collisionDataは、マップの壁を作る。例えば１なら壁だが、０なら壁ではない。
         map.collisionData = [
         [ 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
         [ 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1], 
